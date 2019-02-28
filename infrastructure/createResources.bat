b21e73b9dd0404e35105aec7761e7378e5b0c9a0
Juan Carlos Andreu <jcandreu@kaya-consulting.com>
2019-03-01 00:10:55 +0100

    got pulumi configured

--- infrastructure/createResources.bat
+++ infrastructure/createResources.bat
@@ -1,2 +1,6 @@
 REM THIS IS FOR WINDOWS, NOT SO HARD TO PORT TO LINUX/MAC
-(docker run -i --env-file ./.env  andreujuanc/pulumi-azure-tools:latest  /bin/bash -c "bash ") < resources.infra
+REM https://github.com/docker/cli/pull/1273 
+(docker run -i^
+    --env-file ./.env^
+    -v  ~\source\repos\andreujuanc\remindr\infrastructure\src:/infrasource^
+    andreujuanc/pulumi-azure-tools:latest  /bin/bash -c "bash ") < resources.infra
