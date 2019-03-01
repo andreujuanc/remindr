8a73b58acfcae00dd3789c44d7ef3cbc797f94b0
Juan Carlos Andreu <jcandreu@kaya-consulting.com>
2019-03-01 13:51:55 +0100

    perfect!

--- infrastructure/createResources.bat
+++ infrastructure/createResources.bat
@@ -2,5 +2,5 @@ REM THIS IS FOR WINDOWS, NOT SO HARD TO PORT TO LINUX/MAC
 REM https://github.com/docker/cli/pull/1273 
 (docker run -i^
     --env-file ./.env^
-    -v  ~\source\repos\andreujuanc\remindr\infrastructure\src:/infrasource^
+    -v  ~\source\repos\andreujuanc\remindr\infrastructure\src:/infrasourceTMP^
     andreujuanc/pulumi-azure-tools:latest  /bin/bash -c "bash ") < createResources.commands
