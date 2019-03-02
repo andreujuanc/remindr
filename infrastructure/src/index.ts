8959d3e4c922de12d9a26fb779d2bd00018a3ef2
Juan Carlos Andreu <jcandreu@kaya-consulting.com>
2019-03-02 21:27:42 +0100

    got helm working . need to change image now

--- infrastructure/src/index.ts
+++ infrastructure/src/index.ts
@@ -9,18 +9,30 @@ import * as helm from "@pulumi/kubernetes/helm";
 import * as k8s from "@pulumi/kubernetes";
 import { k8sCluster, k8sProvider } from "./cluster";
 
-const apache = new helm.v2.Chart(
-    "apache",
-    {
-        repo: "bitnami",
-        chart: "apache",
-        version: "1.0.0"
-    },
+
+// const lco: helm.v2.LocalChartOpts = {
+//     path: './charts/remindr',
+// };
+const appChart: helm.v2.ChartOpts = {
+    chart: 'bitnami/node',
+    values: {
+        // image: {
+        //     repository: 'ehazlett/docker-demo' 
+        // },
+        service: {
+            type: 'LoadBalancer'
+        }
+    }
+}
+
+const remindrChart = new helm.v2.Chart(
+    "remindr",
+    appChart,
     { providers: { kubernetes: k8sProvider } }
 );
 
 export let cluster = k8sCluster.name;
-export let kubeConfig = k8sCluster.kubeConfigRaw;
-export let serviceIP = apache
-    .getResourceProperty("v1/Service", "apache-apache", "status")
-.apply(status => status.loadBalancer.ingress[0].ip);
+//export let kubeConfig = k8sCluster.kubeConfigRaw;
+export let serviceIP = remindrChart
+    .getResourceProperty("v1/Service", "remindr-node", "status")
+    .apply(status => status.loadBalancer.ingress[0].ip);
