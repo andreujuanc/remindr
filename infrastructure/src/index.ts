4d590b42eab5523e6c25829317da8fbca7c8d74b
Juan Carlos Andreu <jcandreu@kaya-consulting.com>
2019-03-02 12:08:43 +0100

    deployed test helm successfully

--- infrastructure/src/index.ts
+++ infrastructure/src/index.ts
@@ -5,22 +5,22 @@ const resourceGroupName = new azure.core.ResourceGroup("Reminder", {
     location: "westus2",
 });
 
-// import * as helm from "@pulumi/kubernetes/helm";
-// import * as k8s from "@pulumi/kubernetes";
+import * as helm from "@pulumi/kubernetes/helm";
+import * as k8s from "@pulumi/kubernetes";
 import { k8sCluster, k8sProvider } from "./cluster";
 
-// const apache = new helm.v2.Chart(
-//     "apache",
-//     {
-//         repo: "bitnami",
-//         chart: "apache",
-//         version: "1.0.0"
-//     },
-//     { providers: { kubernetes: k8sProvider } }
-// );
+const apache = new helm.v2.Chart(
+    "apache",
+    {
+        repo: "bitnami",
+        chart: "apache",
+        version: "1.0.0"
+    },
+    { providers: { kubernetes: k8sProvider } }
+);
 
 export let cluster = k8sCluster.name;
 export let kubeConfig = k8sCluster.kubeConfigRaw;
-// export let serviceIP = apache
-//     .getResourceProperty("v1/Service", "apache-apache", "status")
-// .apply(status => status.loadBalancer.ingress[0].ip);
+export let serviceIP = apache
+    .getResourceProperty("v1/Service", "apache-apache", "status")
+.apply(status => status.loadBalancer.ingress[0].ip);
