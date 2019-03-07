7c21c2ccbe17e7c46cc86f0e2db33e25df246bac
Juan Carlos Andreu <andreujuanc@gmail.com>
2019-03-07 11:53:51 +0100

    setting env vars

--- infrastructure/cli.bat
+++ infrastructure/cli.bat
@@ -3,4 +3,5 @@ docker run^
     -it^
     --env-file ./.env^
     -v  ~\source\repos\andreujuanc\remindr\infrastructure\src:/infrasourceTMP^
-    andreujuanc/pulumi-azure-tools:latest
+    andreujuanc/pulumi-azure-tools:latest^
+    /bin/bash -c "export KUBECONFIG=/infrasourceTMP/kubeconfig.yaml&& bash"
