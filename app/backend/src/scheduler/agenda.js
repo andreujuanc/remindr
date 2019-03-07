196bd8a11c5ff2fb1e6d5d83b786dfe876f6d467
Juan Carlos Andreu <jcandreu@kaya-consulting.com>
2019-03-07 16:18:04 +0100

    cleanup

--- app/backend/src/scheduler/agenda.js
+++ app/backend/src/scheduler/agenda.js
@@ -4,6 +4,7 @@ const Agenda = require('agenda');
 const { DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME = 'remindr-agenda' } = process.env;
 const DBAUTH = typeof DATABASE_USER !== 'undefined' ? `${DATABASE_USER}:${DATABASE_PASSWORD}@` : '';
 const connectionOpts = { db: { address: `mongodb://${DBAUTH}${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`, collection: 'agendaJobs' } };
+
 logger.info(`MongoDb: ${connectionOpts.db.address}`);
 
 module.exports = function () {
